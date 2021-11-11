b018908ae05343476eea7e298ff7eb5e3a033129
poodzia <poodzia@gmail.com>
2021-11-11 12:25:14 +0800

    added register logo fix

--- resources/js/Jetstream/AuthenticationCardLogo.vue
+++ resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <inertia-link :href="'/'">
-        <img src="/img/logo1.png" class="w-25 h-28"/>
-    </inertia-link>
+  <inertia-link :href="'/'">
+    <img src="/img/logo3.png" class="w-25 h-28 mt-10" />
+  </inertia-link>
 </template>
